7c33767b23f5fba173896cf81ee53f11b08d250e
iammahir2020 <1810659@iub.edu.bd>
2022-04-25 21:53:17 +0600

    adding heroku

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/**
+ * --------------------
+ * One time for our computer
+ * --------------------
+ * 1. Create Heroku account
+ * 2. verify email
+ * 3. install heroku CLI
+ * 4. heroku login
+ * --------------------
+ * For each project one time
+ * --------------------
+ * 1. heroku create
+ *
+ */
